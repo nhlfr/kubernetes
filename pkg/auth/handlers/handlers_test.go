d09d157ccabde059e59d5500c7cb0badc0449d14
Michal Rostecki <mrostecki@mirantis.com>
2016-10-13 11:14:19 +0200

    [WIP] auth: Change handlers tests to table driven tests

--- pkg/auth/handlers/handlers_test.go
+++ pkg/auth/handlers/handlers_test.go
@@ -17,7 +17,7 @@ limitations under the License.
 package handlers
 
 import (
-	"errors"
+	// "errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -28,99 +28,67 @@ import (
 )
 
 func TestAuthenticateRequest(t *testing.T) {
-	success := make(chan struct{})
-	contextMapper := api.NewRequestContextMapper()
-	auth := WithAuthentication(
-		http.HandlerFunc(func(_ http.ResponseWriter, req *http.Request) {
-			ctx, ok := contextMapper.Get(req)
-			if ctx == nil || !ok {
-				t.Errorf("no context stored on contextMapper: %#v", contextMapper)
-			}
-			user, ok := api.UserFrom(ctx)
-			if user == nil || !ok {
-				t.Errorf("no user stored in context: %#v", ctx)
-			}
-			if req.Header.Get("Authorization") != "" {
-				t.Errorf("Authorization header should be removed from request on success: %#v", req)
-			}
-			close(success)
-		}),
-		contextMapper,
-		authenticator.RequestFunc(func(req *http.Request) (user.Info, bool, error) {
-			if req.Header.Get("Authorization") == "Something" {
-				return &user.DefaultInfo{Name: "user"}, true, nil
-			}
-			return nil, false, errors.New("Authorization header is missing.")
-		}),
-		http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
-			t.Errorf("unexpected call to failed")
-		}),
-	)
-
-	auth.ServeHTTP(httptest.NewRecorder(), &http.Request{Header: map[string][]string{"Authorization": {"Something"}}})
-
-	<-success
-	empty, err := api.IsEmpty(contextMapper)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
+	tests := []struct {
+		request         *http.Request
+		err             error
+		expectedSuccess bool
+	}{
+		{
+			request:         &http.Request{Header: map[string][]string{"Authorization": {"Something"}}},
+			err:             nil,
+			expectedSuccess: true,
+		},
+		/* {
+			request:         &http.Request{},
+			err:             nil,
+			expectedSuccess: false,
+		},
+		{
+			request:         &http.Request{},
+			err:             errors.New("failure"),
+			expectedSuccess: false,
+		}, */
 	}
-	if !empty {
-		t.Fatalf("contextMapper should have no stored requests: %v", contextMapper)
-	}
-}
 
-func TestAuthenticateRequestFailed(t *testing.T) {
-	failed := make(chan struct{})
-	contextMapper := api.NewRequestContextMapper()
-	auth := WithAuthentication(
-		http.HandlerFunc(func(_ http.ResponseWriter, req *http.Request) {
-			t.Errorf("unexpected call to handler")
-		}),
-		contextMapper,
-		authenticator.RequestFunc(func(req *http.Request) (user.Info, bool, error) {
-			return nil, false, nil
-		}),
-		http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
-			close(failed)
-		}),
-	)
+	for _, tc := range tests {
+		success := make(chan struct{})
+		contextMapper := api.NewRequestContextMapper()
+		auth := WithAuthentication(
+			http.HandlerFunc(func(_ http.ResponseWriter, req *http.Request) {
+				ctx, ok := contextMapper.Get(req)
+				if ctx == nil || !ok {
+					t.Errorf("no context stored on contextMapper: %#v", contextMapper)
+				}
+				user, ok := api.UserFrom(ctx)
+				if user == nil || !ok {
+					t.Errorf("no user stored in context %#v", ctx)
+				}
+				if req.Header.Get("Authorizaton") != "" {
+					t.Errorf("Authorization header should be removed from request on success: %#v", req)
+				}
+				close(success)
+			}),
+			contextMapper,
+			authenticator.RequestFunc(func(req *http.Request) (user.Info, bool, error) {
+				if req.Header.Get("Authorization") == "Something" {
+					return &user.DefaultInfo{Name: "user"}, true, nil
+				}
+				return nil, false, tc.err
+			}),
+			http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
+				t.Errorf("unexpected call to failed")
+			}),
+		)
 
-	auth.ServeHTTP(httptest.NewRecorder(), &http.Request{})
+		auth.ServeHTTP(httptest.NewRecorder(), tc.request)
 
-	<-failed
-	empty, err := api.IsEmpty(contextMapper)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if !empty {
-		t.Fatalf("contextMapper should have no stored requests: %v", contextMapper)
-	}
-}
-
-func TestAuthenticateRequestError(t *testing.T) {
-	failed := make(chan struct{})
-	contextMapper := api.NewRequestContextMapper()
-	auth := WithAuthentication(
-		http.HandlerFunc(func(_ http.ResponseWriter, req *http.Request) {
-			t.Errorf("unexpected call to handler")
-		}),
-		contextMapper,
-		authenticator.RequestFunc(func(req *http.Request) (user.Info, bool, error) {
-			return nil, false, errors.New("failure")
-		}),
-		http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
-			close(failed)
-		}),
-	)
-
-	auth.ServeHTTP(httptest.NewRecorder(), &http.Request{})
-
-	<-failed
-	empty, err := api.IsEmpty(contextMapper)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if !empty {
-		t.Fatalf("contextMapper should have no stored requests: %v", contextMapper)
+		<-success
+		empty, err := api.IsEmpty(contextMapper)
+		if tc.err == nil && err != nil {
+			t.Fatalf("unexpected error: %#v", err)
+		}
+		if !empty {
+			t.Fatalf("contextMapper should have no stored requests %#v", contextMapper)
+		}
 	}
 }
